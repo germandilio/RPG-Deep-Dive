38dfc143eb63ce21e3a932b406639c2d5d90662c
germandilio <70810123+germandilio@users.noreply.github.com>
2022-02-13 19:50:54 +0300

    Add health mechanic

---
+++ RPG Project/Assets/Scripts/RPG/Combat/Health.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Health : MonoBehaviour
+    {
+        [SerializeField]
+        private float initialHealthPoints = 100f;
+
+        public bool IsDead { get; private set; }
+        private float _currentHealthPoints;
+
+        private Animator _animator;
+        private static readonly int DeadId = Animator.StringToHash("Dead");
+
+        private void Awake()
+        {
+            _currentHealthPoints = initialHealthPoints;
+            _animator = GetComponent<Animator>();
+        }
+
+        public void TakeDamage(float damage)
+        {
+            _currentHealthPoints = Math.Max(_currentHealthPoints - damage, 0);
+            // Debug.Log(_currentHealthPoints);
+            if (_currentHealthPoints == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (IsDead) return;
+            
+            // TODO (13/02/2022) set enable to false on enemy capsule collider
+            _animator.SetTrigger(DeadId);
+            IsDead = true;
+        }
+    }
+}
